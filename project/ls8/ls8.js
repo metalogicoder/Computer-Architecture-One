543137b8acabb034246597e78b666de0d2193f4f
Devon Bacon <devon.bacon@gmail.com>
2018-06-11 19:13:16 -0700

    ls8.js = created and ran mult program

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -10,7 +10,7 @@ function loadMemory() {
 
   // Hardcoded program to print the number 8 on the console
 
-  const program = [ // print8.ls8
+  const print8 = [ // print8.ls8
     "10011001", // LDI R0,8  Store 8 into R0
     "00000000",
     "00001000",
@@ -19,9 +19,24 @@ function loadMemory() {
     "00000001"  // HLT       Halt and quit
   ];
 
+  const mult = [ // multi.ls8
+    "10011001", // LDI R0,8
+    "00000000",
+    "00001000",
+    "10011001", // LDI R1,9
+    "00000001",
+    "00001001",
+    "10101010", // MUL R0,R1 <---
+    "00000000",
+    "00000001",
+    "01000011", // PRN R0
+    "00000000",
+    "00000001", // HLT
+  ]
+
   // Load the program into the CPU's memory a byte at a time
-  for (let i = 0; i < program.length; i++) {
-    cpu.poke(i, parseInt(program[i], 2));
+  for (let i = 0; i < mult.length; i++) {
+    cpu.poke(i, parseInt(mult[i], 2));
   }
 }
 
