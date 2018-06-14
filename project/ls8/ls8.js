f2340192c4695627e30f965e706049a763294337
Devon Bacon <devon.bacon@gmail.com>
2018-06-14 14:50:04 -0700

    ls8.js - modified parseing to ignore comments

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -62,8 +62,9 @@ const filename = argv[0];
 const filedata = fs.readFileSync(filename, "utf8");
 const lines = filedata.trim().split(/[\r\n]+/g);
 
-lines.forEach((bin, idx) => {
-  if(idx) cpu.poke(idx, parseInt(bin.split(' ')[0], 2));
+lines.forEach((line, idx) => {
+  const comment = line.split(' ')[0] === '#';
+  if(!comment) cpu.poke(idx, parseInt(line.split(' ')[0], 2));
 });
 
 // loadMemory(cpu);
